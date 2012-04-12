dfe09e1f2bdd938a2066ea6c8d4fbefd5c3aa84f
Mike Helland <mobydikc@gmail.com>
2012-04-12 15:39:22 -0700

    send's the file in a service, and uses notifications to inform the user.

--- workspace/mediaGoblin/src/com/mediagoblin/Main.java
+++ workspace/mediaGoblin/src/com/mediagoblin/Main.java
@@ -1,7 +1,6 @@
 package com.mediagoblin;
 
 import android.app.Activity;
-import android.content.Intent;
 import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.preference.PreferenceManager;
